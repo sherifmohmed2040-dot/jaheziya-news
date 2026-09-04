331815bb4cae9b645ff9e97fb952de49b4b84739
Sherif Mohamed <sherifmohmed2040-dot@users.noreply.github.com>
2026-09-04 08:43:24 +0400

    Fix media images on Railway

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("news.urls")),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^news_images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
